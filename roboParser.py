2242cedc690b3fb0fa953f0ed9eca23e23b65552
David Brosnan <dbrosn626@gmail.com>
2016-05-07 06:35:25 -0400

    Cleaned up code, implemented ignore, added output formatting

--- roboParser.py
+++ roboParser.py
@@ -5,7 +5,7 @@ import sys
 import urllib2
 import argparse
 
-def parse(urlBase):
+def parse(urlBase, delay, ignore):
 
 	urlRobots = urlBase + "/robots.txt"	
 	resp = urllib2.urlopen(urlRobots).readlines()
@@ -22,13 +22,13 @@ def parse(urlBase):
 			
 		try:
 			robResp = urllib2.urlopen(url)
-			print str(robResp.code) + " -- " + item + " -- " + str(robResp.headers['content-length'])
-			#print "----------"
-			#print robResp.read()
-			#print " -----------"
+			print "{0:3} -- {1:40} -- {2:5}".format(robResp.code, item, str(robResp.headers['content-length']))
 	
 		except urllib2.HTTPError as e:
-				print str(e.code) + " -- " + item	
+			if not ignore:	
+				print "{0:3} -- {1:40} -- {2:5}".format(e.code, item, str(e.headers['content-length']))
+	
+
 
 if __name__ == "__main__":
 
@@ -36,10 +36,20 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Check response codes and sizes for paths listed in a baseURL's robots.txt")
 	parser.add_argument('URL', metavar= 'URL', type=str, nargs=1)
 	parser.add_argument("-d", metavar='delay', type=int, nargs=1, help="Delay in milliseconds")
-	
+	parser.add_argument("--ignore", action='store_true', help="Ignore 4xx and 5xx response codes")	
+
 	args = parser.parse_args()
 
 	url = args.URL.pop()
-	delay = args.d.pop()
+	
+	if args.d is not None:
+		delay = args.d.pop()
+	else:
+		delay = 0
 
-	parse(url)
+	if args.ignore is not None:
+		ignore = args.ignore
+	else:
+		ignore = False
+
+	parse(url, delay, ignore)
